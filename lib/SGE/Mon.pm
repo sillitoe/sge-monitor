ecc179402de09efe0fc294d699dd0fd73faacc6c
sillitoe <ian@sillit.com>
2019-03-15 14:28:06 +0000

    move general functions to utils

--- lib/SGE/Mon.pm
+++ lib/SGE/Mon.pm
@@ -2,12 +2,11 @@ package SGE::Mon;
 use Dancer2;
 use Dancer2::Plugin::Ajax;
 use Dancer2::Plugin::Cache::CHI;
-use XML::Simple qw( :strict );
+use Dancer2::Plugin::DirectoryView;
+use SGE::Mon::Util;
 
 # default template to use for pages
 set layout => 'main';
-
-my $xs = XML::Simple->new();
 
 our $VERSION = '0.1';
 
@@ -30,7 +29,7 @@ get '/jobs_pending' => sub {
 
 # add qstat and config to stash for all template pages
 hook before_template_render => sub {
-    my $tokens = shift;		
+    my $tokens = shift;
 		my $nocache = param "nocache" || 0;
 		my $qstat_data = get_qstat_all( nocache => $nocache );
 
@@ -40,9 +39,24 @@ hook before_template_render => sub {
     $tokens->{config} = config;
 };
 
+# return JSON data structure to describe job detail
+ajax '/job/:job_id' => sub {
+    my $job_id = params->{job_id};
+
+    $job_id =~ /^([0-9]+)$/
+      or die "! Error: expected an integer as job_id (got '$job_id')";
+
+    my $job_id_clean = $1;
+
+    my $job_data = SGE::Mon::Util::parse_qstat_job( $job_id_clean );
+
+    to_json( $job_data );
+};
+
+
 # get qstat data from cache
 sub get_qstat_all {
-		my %params = @_; 
+		my %params = @_;
 		my $nocache = defined $params{nocache} ? $params{nocache} : 0;
     my $cache_key = 'qstat_all';
 
@@ -55,7 +69,7 @@ sub get_qstat_all {
 				else {
 					info "Requested cache refresh";
 				}
-				$qstat_all = process_qstat_all();
+				$qstat_all = SGE::Mon::Util::parse_qstat_all();
         info "Storing qstat data in cache '$cache_key'";
         cache_set $cache_key, $qstat_all;
     }
@@ -64,91 +78,5 @@ sub get_qstat_all {
     }
     return $qstat_all;
 }
-
-# parse information from qstat (XML)
-sub process_qstat_all {
-
-    my $qstat_xml = `qstat -u '*' -f -xml`;
-    my $qstat_ref = $xs->XMLin( $qstat_xml, KeyAttr => {}, ForceArray => [ qr/list$/i ] );
-    my $qstat_queues = $qstat_ref->{'queue_info'}->{'Queue-List'} || [];
-    my $qstat_jobs   = $qstat_ref->{'job_info'}->{'job_list'} || [];
-
-    my @nodes;
-    my @jobs_running;
-    for my $queue (@$qstat_queues) {
-		    my $job_list = $queue->{'job_list'} || [];
-
-        for my $job ( @$job_list ) {
-            push @jobs_running,
-              {
-                owner      => $job->{JB_owner},
-                start_time => $job->{JAT_start_time},
-                job_number => $job->{JB_job_number},
-                slots      => $job->{slots},
-                name       => $job->{JB_name},
-                priority   => $job->{JAT_prio},
-                state      => $job->{state},
-              };
-        }
-        push @nodes,
-          {
-            name        => $queue->{name},
-            arch        => $queue->{arch},
-            slots_used  => $queue->{slots_used},
-            slots_resv  => $queue->{slots_resv},
-            slots_total => $queue->{slots_total},
-            qtype       => $queue->{qtype},
-            state       => $queue->{state},
-          };
-    }
-
-    my @jobs_pending;
-    for my $job (@$qstat_jobs) {
-        push @jobs_pending,
-          {
-            queue_name      => $job->{queue_name},
-            owner           => $job->{JB_owner},
-            job_number      => $job->{JB_job_number},
-            state           => $job->{state},
-            name            => $job->{JB_name},
-            slots           => $job->{slots},
-            priority        => $job->{JAT_prio},
-            submission_time => $job->{JB_submission_time},
-          };
-    }
-
-    return {
-        timestamp    => "" . localtime,
-        qstat        => $qstat_ref,
-        nodes        => \@nodes,
-        jobs_running => \@jobs_running,
-        jobs_pending => \@jobs_pending,
-    };
-}
-
-# return JSON data structure to describe job detail
-ajax '/job/:job_id' => sub {
-    my $job_id = params->{job_id};
-
-    $job_id =~ /^([0-9]+)$/
-      or die "! Error: expected an integer as job_id (got '$job_id')";
-
-    my $job_id_clean = $1;
-
-    my $qstat_xml = `qstat -j $job_id_clean -xml`;
-
-    my $qstat_ref = $xs->XMLin( $qstat_xml, KeyAttr => {}, ForceArray => [] );
-
-    my $qstat_job_fields = $qstat_ref->{'djob_info'}->{'element'};
-
-    my %job_data;
-    for my $qstat_key ( keys %{$qstat_job_fields} ) {
-        my $job_value = $qstat_job_fields->{$qstat_key};
-        ( my $job_key = $qstat_key ) =~ s/^JB_//;
-        $job_data{$job_key} = $job_value unless ref $job_value;
-    }
-
-    to_json( \%job_data );
-};
 
 true;
---
+++ lib/SGE/Mon/Util.pm
@@ -0,0 +1,94 @@
+package SGE::Mon::Util;
+
+use strict;
+use warnings;
+use XML::Simple qw( :strict );
+use namespace::autoclean;
+
+my $xs = XML::Simple->new();
+
+# parse information from qstat (XML)
+sub parse_qstat_all {
+
+    my $qstat_xml = `qstat -u '*' -f -xml`;
+    my $qstat_ref = $xs->XMLin( $qstat_xml, KeyAttr => {}, ForceArray => [ qr/list$/i ] );
+    my $qstat_queues = $qstat_ref->{'queue_info'}->{'Queue-List'} || [];
+    my $qstat_jobs   = $qstat_ref->{'job_info'}->{'job_list'} || [];
+
+    my @nodes;
+    my @jobs_running;
+    for my $queue (@$qstat_queues) {
+		    my $job_list = $queue->{'job_list'} || [];
+
+        for my $job ( @$job_list ) {
+            push @jobs_running,
+              {
+                owner      => $job->{JB_owner},
+                start_time => $job->{JAT_start_time},
+                job_number => $job->{JB_job_number},
+                slots      => $job->{slots},
+                name       => $job->{JB_name},
+                priority   => $job->{JAT_prio},
+                state      => $job->{state},
+              };
+        }
+        push @nodes,
+          {
+            name        => $queue->{name},
+            arch        => $queue->{arch},
+            slots_used  => $queue->{slots_used},
+            slots_resv  => $queue->{slots_resv},
+            slots_total => $queue->{slots_total},
+            qtype       => $queue->{qtype},
+            state       => $queue->{state},
+          };
+    }
+
+    my @jobs_pending;
+    for my $job (@$qstat_jobs) {
+        push @jobs_pending,
+          {
+            queue_name      => $job->{queue_name},
+            owner           => $job->{JB_owner},
+            job_number      => $job->{JB_job_number},
+            state           => $job->{state},
+            name            => $job->{JB_name},
+            slots           => $job->{slots},
+            priority        => $job->{JAT_prio},
+            submission_time => $job->{JB_submission_time},
+          };
+    }
+
+    return {
+        timestamp    => "" . localtime,
+        qstat        => $qstat_ref,
+        nodes        => \@nodes,
+        jobs_running => \@jobs_running,
+        jobs_pending => \@jobs_pending,
+    };
+}
+
+sub parse_qstat_job {
+  my $job_id = shift;
+
+  $job_id =~ m/^[0-9a-ZA-Z]+$/
+    or croak "! Error: string '$job_id' does not look like a valid job ID";
+
+  my $qstat_xml = `qstat -j $job_id -xml`;
+
+  my $qstat_ref = $xs->XMLin( $qstat_xml, KeyAttr => {}, ForceArray => [] );
+
+  my $qstat_job_fields = $qstat_ref->{'djob_info'}->{'element'};
+
+  my %job_data;
+  for my $qstat_key ( keys %{$qstat_job_fields} ) {
+      my $job_value = $qstat_job_fields->{$qstat_key};
+      ( my $job_key = $qstat_key ) =~ s/^JB_//;
+      $job_data{$job_key} = $job_value unless ref $job_value;
+  }
+
+  return \%job_data;
+}
+
+
+1;
